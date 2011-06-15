b82a9e7a26370823ecc3c9211d632e0bc9c3c979
Simon <Simon@LuXoR>
2011-06-15 23:21:03 +0100

    add

---
+++ Project1/src/info/givre/test1/Test1.java
@@ -0,0 +1,12 @@
+package info.givre.test1;
+
+public class Test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("test1");
+	}
+
+}
---
+++ Project2/src/info/givre/test2/Test2.java
@@ -0,0 +1,12 @@
+package info.givre.test2;
+
+public class Test2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("test2");
+	}
+
+}
